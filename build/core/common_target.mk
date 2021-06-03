f79f8bb148fff5204122a374c20a712d46ada3c0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-03 18:18:50 +0200

    build: core: Make set-device-specific-path generic
    * In preparation for AW hardware
    
    Change-Id: I8009c750a09875c0de92216d463fd9248cef810e

---
+++ build/core/common_target.mk
@@ -0,0 +1,9 @@
+# Set device-specific HALs into project pathmap
+define set-device-specific-path
+$(if $(USE_DEVICE_SPECIFIC_$(1)), \
+    $(if $(DEVICE_SPECIFIC_$(1)_PATH), \
+        $(eval path := $(DEVICE_SPECIFIC_$(1)_PATH)), \
+        $(eval path := $(TARGET_DEVICE_DIR)/$(2))), \
+    $(eval path := $(3))) \
+$(call project-set-path,$(2),$(strip $(path)))
+endef
